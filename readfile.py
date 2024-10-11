7ffa8bd6d3e0a8f3822c6e95bd4db1fb4bd5d4ea
supananp <view18.sp@gmail.com>
2024-10-12 00:43:14 +0700

    init

--- readfile.py
+++ readfile.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 # อ่านไฟล์ Excel ที่อยู่ในโฟลเดอร์เดียวกัน
-df = pd.read_excel('ยอดจดทะเบียนรถรวม.xlsx')
+df = pd.read_excel(r'C:\streamlit\streamlit\ยอดจดทะเบียนรถรวม.xlsx')
 
 # แสดงข้อมูล
 st.write(df)
