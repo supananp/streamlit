bda030e56aeb5fb54b243327d537707efda75128
supananp <view18.sp@gmail.com>
2024-10-12 09:23:09 +0700

    init

--- readfile.py
+++ readfile.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
 # ใช้เส้นทางสัมพัทธ์ที่เหมาะสมกับโครงสร้างของโฟลเดอร์ใน repository
-df = pd.read_excel('https://github.com/supananp/streamlit/blob/main/%E0%B8%A2%E0%B8%AD%E0%B8%94%E0%B8%88%E0%B8%94%E0%B8%97%E0%B8%B0%E0%B9%80%E0%B8%9A%E0%B8%B5%E0%B8%A2%E0%B8%99%E0%B8%A3%E0%B8%96%E0%B8%A3%E0%B8%A7%E0%B8%A1.xlsx')
+df = pd.read_excel('C:\streamlit\ยอดจดทะเบียนรถรวม.xlsx', engine='openpyxl')
